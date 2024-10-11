d040545e2686082534b8c2bbbd7223ac9777164d
DIEGO PRECIADO <DIEGO PRECIADO@DIEGOPRECIADO>
2024-10-11 00:03:43 -0600

    Primer Avance del programa con dos boolean y la fecha actual

--- src/programaanclado/ProgramaAnclado.java
+++ src/programaanclado/ProgramaAnclado.java
@@ -1,23 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package programaanclado;
 
-/**
- *
- * @author DIEGO PRECIADO
- */
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ProgramaAnclado {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        System.out.println("Hola");
-        System.out.println("Hola a todo el mundo, es una prueba de git");
+        boolean resultadoOR = or(true, true);
+        System.out.println("Resultado OR: " + resultadoOR);
+        
+        String fechaHoraActual = obtenerFechaHoraActual();
+        System.out.println("La fecha actual es: " + fechaHoraActual);
+
+        
+        
+    }
+    
+    private static boolean or(boolean a, boolean b) {
+        return a || b;
+    }
+
+    private static String obtenerFechaHoraActual() {
+        LocalDateTime ahora = LocalDateTime.now();        
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");        
+        return ahora.format(formato);    
     }
     
 }
