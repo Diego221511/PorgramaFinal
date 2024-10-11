fa8f62be96252f74a7358fdf8f9a6851aa53df2a
DIEGO PRECIADO <DIEGO PRECIADO@DIEGOPRECIADO>
2024-10-11 00:06:52 -0600

    Prueba 2 de guardado

--- src/programaanclado/ProgramaAnclado.java
+++ src/programaanclado/ProgramaAnclado.java
@@ -4,27 +4,30 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ProgramaAnclado {
-
+    
     public static void main(String[] args) {
-        
         boolean resultadoOR = or(true, true);
         System.out.println("Resultado OR: " + resultadoOR);
         
-        String fechaHoraActual = obtenerFechaHoraActual();
+        String fechaHoraActual = obtenerfechaHoraActual();
         System.out.println("La fecha actual es: " + fechaHoraActual);
-
-        
-        
+        System.out.println("Hola, prueba 2");
     }
-    
-    private static boolean or(boolean a, boolean b) {
+
+    public static boolean or(boolean a, boolean b) {
         return a || b;
     }
 
-    private static String obtenerFechaHoraActual() {
-        LocalDateTime ahora = LocalDateTime.now();        
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");        
-        return ahora.format(formato);    
+    private static String obtenerfechaHoraActual() {
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return ahora.format(formato);
+
     }
     
-}
+    
+    
+    
+    
+    
+    }
