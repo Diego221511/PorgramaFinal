44bf19abb961bbe777d8f85a3e8dd7ffc319b584
DIEGO PRECIADO <DIEGO PRECIADO@DIEGOPRECIADO>
2024-10-10 23:21:04 -0600

    Principio

---
+++ src/programaanclado/ProgramaAnclado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programaanclado;
+
+/**
+ *
+ * @author DIEGO PRECIADO
+ */
+public class ProgramaAnclado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
